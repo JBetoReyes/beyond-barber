23640207da4c26a3fa75cc48391831241f96480b
JBetoReyes <josereyes55@hotmail.com>
2023-10-25 20:50:07 -0600

    adding a theme switcher

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,43 @@
-import './style.css'
-import { setupCounter } from './counter.ts'
+const themeSwitcher = document.querySelector(".theme-switcher");
+let activeTheme = localStorage.getItem("theme") as "system" | "light" | "dark";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (themeSwitcher) {
+    themeSwitcher.addEventListener("click", (e) => {
+        const target = e.target as HTMLElement;
+        if (target && target.tagName != "INPUT") {
+            return;
+        }
+
+        const themeToActivate = target.id as "system" | "light" | "dark";
+        changeTheme(themeToActivate);
+    });
+}
+
+
+
+function changeTheme(theme: "system" | "light" | "dark") {
+    document.body.classList.add("theme-switch");
+    if (theme === "system") {
+        localStorage.removeItem("theme");
+        document.body.removeAttribute("data-theme");
+    } else {
+        localStorage.setItem("theme", theme);
+        document.body.setAttribute("data-theme", theme);
+    }
+
+    document.body.addEventListener(
+        "transitionend",
+        () => {
+            document.body.classList.remove("theme-switch");
+        },
+        {}
+    );
+}
+
+window.onload = () => {
+    if (activeTheme) {
+        const inputElement = document.getElementById(activeTheme) as HTMLInputElement;
+        inputElement.checked = true;
+        changeTheme(activeTheme);
+    }
+};
